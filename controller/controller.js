b8ab53ec26a3fa9fc7b27dc78571093950066367
unloccode <unloccode@gmail.com>
2021-06-27 11:07:52 +0300

    Added functions to controller.js, GET USER, GET ALL USERS, DELETE

--- controller/controller.js
+++ controller/controller.js
@@ -13,6 +13,7 @@ exports.createUser = (req, res) => {
         //save to db
         User.create(user, {
             attributes: [
+                'id',
                 'username',
                 'email',
                 'town',
@@ -29,3 +30,70 @@ exports.createUser = (req, res) => {
         });
     }
 }
+
+exports.getUser = (req, res) => {
+    Customer.findByPk(req.params.id, {
+        attributes: [
+            'id',
+            'username',
+            'email',
+            'age',
+            'town',
+            'password'
+        ]
+    }).then(user=>{
+        res.status(200).json(user);
+    }).catch(error=>{
+        console.log(error);
+        res.status(500).json({
+            message: "Error",
+            error: error
+        });
+    })
+}
+
+exports.Users = (req, res) => {
+    try{
+        User.findAll({
+            attributes: [
+                'id',
+                'username',
+                'email',
+                'age',
+                'town',
+                'password'
+            ]
+        }).then(users=>{
+            res.status(200).json(users);
+        });
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            message: "Error",
+            error: error
+        });
+    }
+}
+
+exports.deleteUser = async(req, res)=>{
+    try{
+        //delete a user using Pk
+        let userId = req.params.id;
+        let user = await User.findByPk(userId);
+        //check
+        if(!user){
+            res.status(400).json({
+                message: "Does not exist a USER with id=" + userId,
+                error: "404"
+            });
+        }else{
+            await user.destroy();
+            res.status(200);
+        }
+    }catch(error){
+        res.status(500).json({
+            message: "Error -> Cannot delete a USER with id=" + req.params.id,
+            error: error.message
+        });
+    }
+})
